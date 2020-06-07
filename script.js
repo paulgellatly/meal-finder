01c1c8847f2bb5c322940f3c5ee9b29ab479c74b
Paul G <paulgellatly@icloud.com>
2020-06-07 19:30:42 +0100

    finished search and display from API

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const search = document.getElementById('search'),
 	submit = document.getElementById('submit'),
 	random = document.getElementById('random'),
-	mealEl = document.getElementById('meals'),
+	mealsEl = document.getElementById('meals'),
 	resultHeading = document.getElementById('result-heading'),
 	single_mealEl = document.getElementById('single-meal');
 
@@ -23,7 +23,27 @@ function searchMeal(e) {
 			.then((res) => res.json())
 			.then((data) => {
 				console.log(data);
+				resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+
+				if (data.meals === null) {
+					resultHeading.innerHTML = `<p>There are no search results. Try again</p>`;
+				} else {
+					mealsEl.innerHTML = data.meals
+						.map(
+							(meal) => `
+                        <div class="meal">
+                            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                            <div class="meal-info" data-mealID="${meal.idMead}">
+                                <h3>${meal.strMeal}</h3>
+                            </div>
+                        </div>
+                    `
+						)
+						.join('');
+				}
 			});
+		// Clear search text
+		search.value = '';
 	} else {
 		alert('Please enter a search value');
 	}
